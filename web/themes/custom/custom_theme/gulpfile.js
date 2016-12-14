96e7385152b74b272d257462e956858fc58038ab
Gerardo Hernández <edgewl2@gmail.com>
2016-12-14 16:05:25 -0600

    Integration PatternLab Complete

--- web/themes/custom/custom_theme/gulpfile.js
+++ web/themes/custom/custom_theme/gulpfile.js
@@ -1,30 +1,32 @@
 var promise = require('es6-promise');
-var gulp         = require('gulp');
-var sass         = require('gulp-sass');
-var postcss      = require('gulp-postcss');
-var sourcemaps   = require('gulp-sourcemaps');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('autoprefixer');
-var bower        = require('gulp-bower');
-var runSequence  = require('run-sequence');
-var clean        = require('gulp-clean');
-var chmod = require('gulp-chmod');
+var bower = require('gulp-bower');
+var runSequence = require('run-sequence');
+var clean = require('gulp-clean');
 
-var input = './sass/**/*.scss';
-var output = './css';
 var autoprefixerOptions = {
-    browsers: ['last 1 version']
+    browsers: ['last 2 versions']
 };
 var processors = [
     autoprefixer(autoprefixerOptions),
 ];
-var bowerComponents = '../../../../bower_components';
-var assetLibraries = '../../../libraries';
-var shPLPostInstall = '../../../../scripts/drupalvm';
-var patternLab = './pattern-lab';
+// Source/destinations.
+var source = './patternlab/source/';
+var input = source + 'sass/style.scss';
+var output = source + 'css';
+var bowerComponents = 'bower_components';
+var patternlabLibraries = source + 'libraries';
+var drupalLibraries = '../../../libraries';
+var drupalTheme = './';
+var bootstrapLibrary = 'bootstrap';
 
 promise.polyfill();
 
-gulp.task('prod', ['bower'], function () {
+gulp.task('prod', ['bower', 'assets'], function () {
     return gulp
         .src(input)
         .pipe(sass({
@@ -32,10 +34,11 @@ gulp.task('prod', ['bower'], function () {
             outputStyle: 'compressed'
         }).on('error', sass.logError))
         .pipe(postcss(processors))
-        .pipe(gulp.dest(output));
+        .pipe(gulp.dest(output))
+        .pipe(gulp.dest(drupalTheme + output));
 });
 
-gulp.task('dev', ['bower'], function () {
+gulp.task('dev', ['bower', 'assets'], function () {
     return gulp
         .src(input)
         .pipe(sourcemaps.init())
@@ -45,92 +48,117 @@ gulp.task('dev', ['bower'], function () {
         }).on('error', sass.logError))
         .pipe(postcss(processors))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(output));
+        .pipe(gulp.dest(output))
+        .pipe(gulp.dest(drupalTheme + output));
 });
 
 gulp.task('watch', function () {
     return gulp
         .watch(input, ['dev'])
-        .on('change', function(event) {
+        .on('change', function (event) {
             console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
         });
 });
 
-gulp.task('bower', function(callback) {
+gulp.task('assets', function (callback) {
     runSequence(
-        'permission',
-        'run-bower',
-        ['clean-libraries', 'clean-theme-bootstrap', 'clean-theme-vendor'],
-        ['bootstrap', 'dropzone', 'imagesloaded', 'jquery-colorbox', 'masonry', 'matchHeight', 'patternlab'],
+        ['clean-images', 'clean-javascript'],
+        ['images', 'javascript'],
         callback
     );
 });
 
-gulp.task('permission', function () {
-    return gulp
-        .src(shPLPostInstall + '/patternlab-postinstall.sh')
-        .pipe(chmod({
-            owner: {
-                read: true,
-                write: true,
-                execute: true
-            },
-            group: {
-                execute: true
-            },
-            others: {
-                execute: true
-            }
-        }))
-        .pipe(gulp.dest(shPLPostInstall));
+gulp.task('bower', function (callback) {
+    runSequence(
+        'run-bower',
+        ['clean-pl-css', 'clean-drupal-css', 'clean-pl-libraries', 'clean-drupal-libraries', 'clean-pl-bootstrap', 'clean-drupal-bootstrap'],
+        ['bootstrap', 'jquery', 'dropzone', 'imagesloaded', 'jquery-colorbox', 'masonry', 'matchHeight'],
+        callback
+    );
 });
 
-gulp.task('run-bower', function() {
+gulp.task('run-bower', function () {
     return bower(bowerComponents);
 });
 
 // Move around some files from the bower folder into proper destinations.
-gulp.task('bootstrap', function() {
-    return gulp.src(bowerComponents + '/bootstrap-sass/**/*.*', { base: bowerComponents + '/bootstrap-sass' })
-        .pipe(gulp.dest('./bootstrap'));
+gulp.task('bootstrap', function () {
+    return gulp.src(bowerComponents + '/bootstrap-sass/**/*.*', {base: bowerComponents + '/bootstrap-sass'})
+        .pipe(gulp.dest(source + bootstrapLibrary))
+        .pipe(gulp.dest(drupalTheme + bootstrapLibrary));
 });
-gulp.task('dropzone', function() {
-    return gulp.src(bowerComponents + '/dropzone/**/*.*', { base: bowerComponents })
-        .pipe(gulp.dest(assetLibraries));
+gulp.task('dropzone', function () {
+    return gulp.src(bowerComponents + '/dropzone/**/*.*', {base: bowerComponents})
+        .pipe(gulp.dest(patternlabLibraries))
+        .pipe(gulp.dest(drupalLibraries));
 });
-gulp.task('imagesloaded', function() {
-    return gulp.src(bowerComponents + '/imagesloaded/**/*.*', { base: bowerComponents })
-        .pipe(gulp.dest(assetLibraries));
+gulp.task('imagesloaded', function () {
+    return gulp.src(bowerComponents + '/imagesloaded/**/*.*', {base: bowerComponents})
+        .pipe(gulp.dest(patternlabLibraries))
+        .pipe(gulp.dest(drupalLibraries));
 });
-gulp.task('jquery-colorbox', function() {
-    return gulp.src(bowerComponents + '/jquery-colorbox/**/*.*', { base: bowerComponents + '/jquery-colorbox' })
-        .pipe(gulp.dest(assetLibraries + '/colorbox'));
+gulp.task('jquery-colorbox', function () {
+    return gulp.src(bowerComponents + '/jquery-colorbox/**/*.*', {base: bowerComponents + '/jquery-colorbox'})
+        .pipe(gulp.dest(patternlabLibraries + '/colorbox'))
+        .pipe(gulp.dest(drupalLibraries + '/colorbox'));
 });
-gulp.task('masonry', function() {
-    return gulp.src(bowerComponents + '/masonry/**/*.*', { base: bowerComponents })
-        .pipe(gulp.dest(assetLibraries));
+gulp.task('masonry', function () {
+    return gulp.src(bowerComponents + '/masonry/**/*.*', {base: bowerComponents})
+        .pipe(gulp.dest(patternlabLibraries))
+        .pipe(gulp.dest(drupalLibraries));
 });
-gulp.task('matchHeight', function() {
-    return gulp.src(bowerComponents + '/matchHeight/**/*.*', { base: bowerComponents })
-        .pipe(gulp.dest('./vendor'));
+gulp.task('matchHeight', function () {
+    return gulp.src(bowerComponents + '/matchHeight/**/*.*', {base: bowerComponents})
+        .pipe(gulp.dest(patternlabLibraries))
+        .pipe(gulp.dest(drupalLibraries));
 });
-gulp.task('patternlab', function() {
-    return gulp.src(bowerComponents + '/edition-php-drupal-standard/**/*', { base: bowerComponents + '/edition-php-drupal-standard'})
-        .pipe(gulp.dest(patternLab));
+gulp.task('jquery', function () {
+    return gulp.src(bowerComponents + '/jquery/**/*.*', {base: bowerComponents})
+        .pipe(gulp.dest(patternlabLibraries));
+});
+
+// Move assets.
+gulp.task('images', function () {
+    return gulp.src(source + 'images/**/*.*')
+        .pipe(gulp.dest(drupalTheme + 'images'));
+});
+gulp.task('javascript', function () {
+    return gulp.src(source + 'js/**/*.*')
+        .pipe(gulp.dest(drupalTheme + 'js'));
 });
 
 // Clean destinations.
-gulp.task('clean-libraries', function() {
-    return gulp.src(assetLibraries, {read: false})
+gulp.task('clean-pl-css', function () {
+    return gulp.src(output, {read: false})
+        .pipe(clean());
+});
+gulp.task('clean-drupal-css', function () {
+    return gulp.src(drupalTheme + output + '/style.css', {read: false})
         .pipe(clean({force: true}));
 });
-gulp.task('clean-theme-bootstrap', function() {
-    return gulp.src('./bootstrap', {read: false})
+gulp.task('clean-pl-libraries', function () {
+    return gulp.src(patternlabLibraries, {read: false})
         .pipe(clean());
 });
-gulp.task('clean-theme-vendor', function() {
-    return gulp.src('./vendor', {read: false})
+gulp.task('clean-drupal-libraries', function () {
+    return gulp.src(drupalLibraries, {read: false})
+        .pipe(clean({force: true}));
+});
+gulp.task('clean-pl-bootstrap', function () {
+    return gulp.src(source + bootstrapLibrary, {read: false})
         .pipe(clean());
+});
+gulp.task('clean-drupal-bootstrap', function () {
+    return gulp.src(drupalTheme + bootstrapLibrary, {read: false})
+        .pipe(clean({force: true}));
+});
+gulp.task('clean-images', function () {
+    return gulp.src(drupalTheme + bootstrapLibrary, {read: false})
+        .pipe(clean({force: true}));
+});
+gulp.task('clean-javascript', function () {
+    return gulp.src(drupalTheme + bootstrapLibrary, {read: false})
+        .pipe(clean({force: true}));
 });
 
 gulp.task('default', ['prod']);
